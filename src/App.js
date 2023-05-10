e48410858978d92429b6f749b4f1195c37a01e02
citec-47 <mauricendonyi40@gmail.com>
2023-05-10 12:35:10 -0700

    edit remove

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/navbar';
-import BookPage from './pages/home';
-import Categories from './pages/category';
+import BookPage from './Homepage/home';
+import Categories from './Homepage/category';
 
 function App() {
   return (
