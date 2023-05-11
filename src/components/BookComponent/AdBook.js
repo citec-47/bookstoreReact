ce97560bfbce5efd780af72b341e52600f8a58fd
citec-47 <mauricendonyi40@gmail.com>
2023-05-11 01:26:06 -0700

    Editing homepage

--- src/components/BookComponent/AdBook.js
+++ src/components/BookComponent/AdBook.js
@@ -38,9 +38,9 @@ function AddMyBookForm() {
         <input type="text" name="author" placeholder="Author name" onChange={changeFunctio} required />
         <select name="category" onChange={changeFunctio} required>
           <option value="" defaultValue>category</option>
-          <option value="Action">Commercial Subjects</option>
-          <option value="Science Fiction">Art Subjects</option>
-          <option value="Economy">Science Subjects</option>
+          <option value="Action">Science</option>
+          <option value="Science Fiction">Art</option>
+          <option value="Economy">Commercial</option>
         </select>
         <input className={styles.butt} type="submit" value="ADD BOOK" />
       </form>
